330ba6fd45fd931c7c94ab6755c06989927a964e
Adan Agic <adan_agic@hotmail.com>
2017-05-31 15:03:30 +0200

    adding animations

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+var $about = $('#about');
+var $technologies = $('#technologies');
+
+$technologies.waypoint(function() {
+  $('#technologies_main_div').addClass('animated lightSpeedIn');
+  $('#technologies_main_div').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+    $('#technologies_main_div').removeClass('animated lightSpeedIn');
+  });
+}, {offset: '70%'});
+
+$about.waypoint(function() {
+  $('#img_about').addClass('animated slideInRight');
+  $('#img_about').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+    $('#img_about').removeClass('animated slideInRight');
+  });
+
+  $('#text_about').addClass('animated slideInLeft');
+  $('#text_about').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+    $('#text_about').removeClass('animated slideInLeft');
+  });
+}, {offset: '70%'});
+
+document.addEventListener('DOMContentLoaded', function(){
+  Typed.new('.welcome-text', {
+  strings: ["Let's be creative"],
+  typeSpeed: 15
+  });
+});
